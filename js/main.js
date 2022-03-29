3061d19bffef4d8b747fee11d75e6dd6259de88a
aliciagsj <aliciagsj@gmail.com>
2022-03-29 10:48:43 +0200

    add function bet

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
 const selectElement = document.querySelector(".js_select");
 const btn = document.querySelector(".js_btn");
 const textResult = document.querySelector(".js_result");
+const balanceResult = document.querySelector(".js_balance");
+const inputBet = document.querySelector(".js_bet");
 
 //2. Declarar funciones y eventos
 
@@ -18,19 +20,26 @@ function winOrLose() {
 
   if (numAleat === numSelect) {
     textResult.innerHTML = "Has ganado el doble de lo apostado :)";
+    resultBalance(true);
   } else {
     textResult.innerHTML = "Has perdido lo apostado :(";
+    resultBalance(false);
   }
 }
 
-function resultBalance() {}
+function resultBalance(win) {
+  const bet = parseInt(inputBet.value);
+
+  if (win) {
+    balanceResult.innerHTML = parseInt(balanceResult.innerHTML) + bet * 2;
+  } else {
+    balanceResult.innerHTML = parseInt(balanceResult.innerHTML) - bet;
+  }
+}
 
 function handleClickBtn(event) {
   event.preventDefault();
-
-  resultBalance();
   winOrLose();
 }
 
 btn.addEventListener("click", handleClickBtn);
-console.log(numAleat);
